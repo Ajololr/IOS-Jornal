76b88da09a2e9f1f1e5f805c5f321ec7bb4d0e50
Ilya Androsov <ilya.androsau@itechart-group.com>
2021-03-01 19:36:55 +0300

    add settings for font and color

--- lab/MapViewController.swift
+++ lab/MapViewController.swift
@@ -13,9 +13,7 @@ class MapViewController: UIViewController, GMSMapViewDelegate {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
-        // Create a GMSCameraPosition that tells the map to display the
-        // coordinate -33.86,151.20 at zoom level 6.
+        
         let camera = GMSCameraPosition.camera(withLatitude: 53.9006, longitude: 27.5590, zoom: 6.0)
         mapView = GMSMapView.map(withFrame: self.view.frame, camera: camera)
         mapView.delegate = self
@@ -23,9 +21,179 @@ class MapViewController: UIViewController, GMSMapViewDelegate {
     }
 
     override func viewDidAppear(_ animated: Bool) {
+        if (SettingsViewController.blackTheme) {
+        do {
+            try mapView.mapStyle = GMSMapStyle(jsonString: """
+[
+  {
+    "elementType": "geometry",
+    "stylers": [
+      {
+        "color": "#242f3e"
+      }
+    ]
+  },
+  {
+    "elementType": "labels.text.fill",
+    "stylers": [
+      {
+        "color": "#746855"
+      }
+    ]
+  },
+  {
+    "elementType": "labels.text.stroke",
+    "stylers": [
+      {
+        "color": "#242f3e"
+      }
+    ]
+  },
+  {
+    "featureType": "administrative.locality",
+    "elementType": "labels.text.fill",
+    "stylers": [
+      {
+        "color": "#d59563"
+      }
+    ]
+  },
+  {
+    "featureType": "poi",
+    "elementType": "labels.text.fill",
+    "stylers": [
+      {
+        "color": "#d59563"
+      }
+    ]
+  },
+  {
+    "featureType": "poi.park",
+    "elementType": "geometry",
+    "stylers": [
+      {
+        "color": "#263c3f"
+      }
+    ]
+  },
+  {
+    "featureType": "poi.park",
+    "elementType": "labels.text.fill",
+    "stylers": [
+      {
+        "color": "#6b9a76"
+      }
+    ]
+  },
+  {
+    "featureType": "road",
+    "elementType": "geometry",
+    "stylers": [
+      {
+        "color": "#38414e"
+      }
+    ]
+  },
+  {
+    "featureType": "road",
+    "elementType": "geometry.stroke",
+    "stylers": [
+      {
+        "color": "#212a37"
+      }
+    ]
+  },
+  {
+    "featureType": "road",
+    "elementType": "labels.text.fill",
+    "stylers": [
+      {
+        "color": "#9ca5b3"
+      }
+    ]
+  },
+  {
+    "featureType": "road.highway",
+    "elementType": "geometry",
+    "stylers": [
+      {
+        "color": "#746855"
+      }
+    ]
+  },
+  {
+    "featureType": "road.highway",
+    "elementType": "geometry.stroke",
+    "stylers": [
+      {
+        "color": "#1f2835"
+      }
+    ]
+  },
+  {
+    "featureType": "road.highway",
+    "elementType": "labels.text.fill",
+    "stylers": [
+      {
+        "color": "#f3d19c"
+      }
+    ]
+  },
+  {
+    "featureType": "transit",
+    "elementType": "geometry",
+    "stylers": [
+      {
+        "color": "#2f3948"
+      }
+    ]
+  },
+  {
+    "featureType": "transit.station",
+    "elementType": "labels.text.fill",
+    "stylers": [
+      {
+        "color": "#d59563"
+      }
+    ]
+  },
+  {
+    "featureType": "water",
+    "elementType": "geometry",
+    "stylers": [
+      {
+        "color": "#17263c"
+      }
+    ]
+  },
+  {
+    "featureType": "water",
+    "elementType": "labels.text.fill",
+    "stylers": [
+      {
+        "color": "#515c6d"
+      }
+    ]
+  },
+  {
+    "featureType": "water",
+    "elementType": "labels.text.stroke",
+    "stylers": [
+      {
+        "color": "#17263c"
+      }
+    ]
+  }
+]
+""")
+            } catch {
+                print("Error setting maps style")
+            }
+            
+        }
+        
         for student in StudentTableViewController.students {
             let marker = GMSMarker()
-//            marker.position = CLLocationCoordinate2D(latitude: 53.9006, longitude: 27.5590)
             marker.position = CLLocationCoordinate2D(latitude: Double(student.latitude) ?? 0, longitude: Double(student.longitude) ?? 0)
             marker.title = student.firstName + " " + student.lastName + "'s home"
             let dateFromatter =  DateFormatter();
--- lab/SettingsViewController.swift
+++ lab/SettingsViewController.swift
@@ -6,22 +6,101 @@
 //
 
 import UIKit
+import GoogleMaps
 
 class SettingsViewController: UIViewController {
 
-    @IBOutlet weak var themeMode: UISegmentedControl!
+    static var appFontSize : CGFloat = CGFloat( SettingsViewController.appFontSize)
+    static var blackTheme = false
+    var picker = UIColorPickerViewController()
+    
+    @IBOutlet weak var themeSwitch: UISwitch!
     @IBOutlet weak var fontSizeSelector: UISlider!
+    @IBOutlet weak var changeColorButton: UIButton!
+    @IBOutlet weak var fontSizeLabel: UILabel!
+    @IBOutlet weak var darkModeLabel: UILabel!
+    @IBOutlet weak var logoutButton: UIBarButtonItem!
+    @IBOutlet weak var fontSlider: UISlider!
+    
+//    override var preferredStatusBarStyle: UIStatusBarStyle {
+//        if SettingsViewController.blackTheme{
+//            return .lightContent
+//        } else {
+//            return .darkContent
+//        }
+//    }
+    
+    func changeFont(){
+        UILabel.appearance().font = darkModeLabel.font.withSize(CGFloat(SettingsViewController.appFontSize))
+        fontSizeLabel.text = "Font size: \(Int(SettingsViewController.appFontSize))"
+        fontSizeLabel.font = fontSizeLabel.font.withSize(CGFloat(SettingsViewController.appFontSize))
+        darkModeLabel.font = darkModeLabel.font.withSize(CGFloat(SettingsViewController.appFontSize))
+        changeColorButton.titleLabel?.font = changeColorButton.titleLabel?.font.withSize(CGFloat(SettingsViewController.appFontSize))
+    }
+    
+    @IBAction func changeColorTap(_ sender: Any) {
+        self.present(picker, animated: true, completion: nil)
+    }
+    
+    @IBAction func fontValueChanged(_ sender: UISlider) {
+        SettingsViewController.appFontSize = CGFloat(sender.value)
+        changeFont()
+    }
+    
+    @IBAction func themeSwitchChanged(_ sender: UISwitch) {
+        if sender.isOn {
+            SettingsViewController.blackTheme = true
+            self.view.overrideUserInterfaceStyle = .dark
+            self.navigationController?.overrideUserInterfaceStyle = .dark
+            self.tabBarController?.overrideUserInterfaceStyle = .dark
+            fontSizeLabel.textColor = #colorLiteral(red: 1.0, green: 1.0, blue: 1.0, alpha: 1.0)
+            darkModeLabel.textColor = #colorLiteral(red: 1.0, green: 1.0, blue: 1.0, alpha: 1.0)
+            setNeedsStatusBarAppearanceUpdate()
+            
+            UIView.appearance().overrideUserInterfaceStyle = .dark
+            UINavigationBar.appearance().overrideUserInterfaceStyle = .dark
+            UITabBar.appearance().overrideUserInterfaceStyle = .dark
+        } else {
+            SettingsViewController.blackTheme = false
+            self.view.overrideUserInterfaceStyle = .light
+            self.navigationController?.overrideUserInterfaceStyle = .light
+            self.tabBarController?.overrideUserInterfaceStyle = .light
+            fontSizeLabel.textColor = #colorLiteral(red: 0, green: 0, blue: 0, alpha: 1)
+            darkModeLabel.textColor = #colorLiteral(red: 0, green: 0, blue: 0, alpha: 1)
+            setNeedsStatusBarAppearanceUpdate()
+            
+            UIView.appearance().overrideUserInterfaceStyle = .light
+            UINavigationBar.appearance().overrideUserInterfaceStyle = .light
+            UITabBar.appearance().overrideUserInterfaceStyle = .light
+            
+        }
+    }
+    
+    func changeOption() {
+        changeFont()
+        fontSizeSelector.value = Float(CGFloat(SettingsViewController.appFontSize))
+        changeFont()
+        
+        themeSwitch.isOn = SettingsViewController.blackTheme
+        themeSwitchChanged(themeSwitch)
+        
+        changeColorButton.tintColor = picker.selectedColor
+        self.navigationController?.navigationBar.tintColor = picker.selectedColor
+        self.tabBarController?.tabBar.tintColor = picker.selectedColor
+    }
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        picker.delegate = self
+        picker.selectedColor =  UIColor.systemBlue
+        changeOption()
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        changeOption()
     }
     
     @IBAction func logout(_ sender: Any) {
-        // ...
-            // after user has successfully logged out
-      
         let storyboard = UIStoryboard(name: "Main", bundle: nil)
         let loginNavController = storyboard.instantiateViewController(identifier: "AuthNavController")
 
@@ -39,4 +118,21 @@ class SettingsViewController: UIViewController {
     }
     */
 
+}
+
+extension SettingsViewController : UIColorPickerViewControllerDelegate {
+    func colorPickerViewControllerDidSelectColor(_ viewController: UIColorPickerViewController) {
+        picker.selectedColor = viewController.selectedColor
+
+        UINavigationBar.appearance().tintColor = viewController.selectedColor
+        UIDatePicker.appearance().tintColor = viewController.selectedColor
+        UIButton.appearance().tintColor = viewController.selectedColor
+        UITabBar.appearance().tintColor = viewController.selectedColor
+        UISlider.appearance().tintColor = viewController.selectedColor
+        logoutButton.tintColor = viewController.selectedColor
+        fontSlider.tintColor = viewController.selectedColor
+       
+        
+        changeOption()
+    }
 }
